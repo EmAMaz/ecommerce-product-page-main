6b67d8a7d7193d40926e44ef693c7b414dacca56
Santiago Maza <santiagomaza33@gmail.com>
2023-06-14 23:39:50 -0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,9 @@
+let image = document.querySelector("#image-dinamica")
+
+image.addEventListener("click", ()=>{
+    
+})
+
+$(window).on('load', function() {
+    $('#exampleModal').modal('show');
+  });
